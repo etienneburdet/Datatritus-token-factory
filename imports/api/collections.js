11708d3aee3a022c0fffb5ba71fe5cfe2be7118f
Etienne Burdet <etienne.burdet@free.fr>
2017-01-22 23:05:22 +0100

    test autofrom NOT Working

--- imports/api/collections.js
+++ imports/api/collections.js
@@ -3,3 +3,21 @@ import { Mongo } from 'meteor/mongo';
 export const Bins = new Mongo.Collection('bins');
 export const Wastes = new Mongo.Collection('wastes');
 export const Days = new Mongo.Collection('days');
+
+Bins.attachSchema(new SimpleSchema({
+  binName: {
+    type: String,
+    label: "Name",
+    max: 50
+  },
+  binWeight: {
+    type: Number,
+    min: 0,
+    label: "Weight of the bin"
+  },
+  wasteType: {
+    type: String,
+    label: "Type of waste in the bin",
+    max: 50
+  }
+}));
